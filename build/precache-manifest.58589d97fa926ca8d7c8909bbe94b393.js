529f9bca4da4676fe58547cebe71b33a1fbe0698
petroKuprianov <petrokuprianov@gmail.com>
2020-09-24 14:16:49 +0300

    build add

---
+++ build/precache-manifest.58589d97fa926ca8d7c8909bbe94b393.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab21ecb89e421ac3ede4149e188c5d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "704266f2613ccb66278b",
+    "url": "/static/css/2.fd7faca8.chunk.css"
+  },
+  {
+    "revision": "c79b0210fb47177a0e0b",
+    "url": "/static/css/main.c608451f.chunk.css"
+  },
+  {
+    "revision": "704266f2613ccb66278b",
+    "url": "/static/js/2.1a89d7b2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1a89d7b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79b0210fb47177a0e0b",
+    "url": "/static/js/main.9df3fb64.chunk.js"
+  },
+  {
+    "revision": "9fd970e1f25c0cd7eb0a",
+    "url": "/static/js/runtime-main.470b33ba.js"
+  },
+  {
+    "revision": "37a75b54218416da18cdf1fa0650c569",
+    "url": "/static/media/Background.37a75b54.jpg"
+  },
+  {
+    "revision": "c1b2293586edb83bcd635cdb4e299109",
+    "url": "/static/media/Logo.c1b22935.jpg"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "/static/media/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "/static/media/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "/static/media/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "/static/media/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "9d63f819011db1fda0a67fcc3c206940",
+    "url": "/static/media/Rubik-Bold.9d63f819.ttf"
+  },
+  {
+    "revision": "36b56224cefc587dcdeea981187e5e2b",
+    "url": "/static/media/Rubik-Light.36b56224.ttf"
+  },
+  {
+    "revision": "f3e8e533dc4ba5c337c8abd1a8935e2b",
+    "url": "/static/media/Rubik-Medium.f3e8e533.ttf"
+  },
+  {
+    "revision": "09556186b81cbb10fd5c43745a7f32a7",
+    "url": "/static/media/Rubik-Regular.09556186.ttf"
+  }
+]);
